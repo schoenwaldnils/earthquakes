5df713707267e28ed77ddb32e7a82220d7463965
Nils Schönwald <nils@schoenwald.media>
2018-03-23 01:20:19 +0100

    move to circleci

--- .scripts/export.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-next build
-next export -o docs
-touch docs/.nojekyll
-touch docs/CNAME
-echo \"earthquakes.schoenwald.media\" >> docs/CNAME
-git add docs
-git commit -m \"build\"
