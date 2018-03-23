81e397a46382653876d35425530dfe032441705f
Nils Schönwald <nils@schoenwald.media>
2018-03-23 01:40:40 +0100

    add comment

--- app/js/utils/calcEarthquakeData.js
+++ app/js/utils/calcEarthquakeData.js
@@ -8,6 +8,7 @@ export default async function (minmagnitude) {
     endYear: today.getFullYear(),
   };
 
+  // https://earthquake.usgs.gov/fdsnws/event/1/
   const params = {
     format: 'geojson',
     starttime: `${options.startYear}-01-01`,
